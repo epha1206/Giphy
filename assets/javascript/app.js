95fee05a7d5b7b173269a1d6d0be1f5c70cf2535
Elvis Pham <epha1206@gmail.com>
2018-03-09 15:55:14 -0500

    added buttons

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,11 @@
-var showTitles = ['The Simpsons', 'Rugrats', 'Dexters Laboratory', 'Animaniacs', 'Futurama', 'South Park', 'Hey Arnold', 'Doug', 'Arthur', 'Rockos Modern Life', 'The Angry Beavers', 'Sailor Moon', 'Rocket Power'];
+var cartoonTitles = ['The Simpsons', 'Rugrats', 'Dexters Laboratory', 'Animaniacs', 'Futurama', 'South Park', 'Hey Arnold', 'Doug', 'Arthur', 'Rockos Modern Life', 'The Angry Beavers', 'Sailor Moon', 'Rocket Power'];
+var currentGif; var pausedGif; var animatedGif; var stillGif;
+
+function createButtons() {
+    $('#cartoonButtons').empty();
+    for(var i = 0; i < cartoonTitles.length; i++){
+        var cartoonBtn = $('<button>').text(cartoonTitles[i]).addClass('cartoonBtn').attr({'data-name': cartoonTitles[i]});
+        $('#cartoonButtons').append(cartoonBtn);
+    }
+    
+}
